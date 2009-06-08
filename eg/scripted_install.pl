43b7182abb6dcd3917b9df2955b73cbc434a0f1e
hdp <hdp@bd8105ee-0ff8-0310-8827-fb3f25b6796d>
2009-06-08 19:46:24 +0000

    missing semicolon, remove ^Ms
    git-svn-id: http://dev.catalyst.perl.org/repos/bast/local-lib/1.000/trunk@6547 bd8105ee-0ff8-0310-8827-fb3f25b6796d

--- eg/scripted_install.pl
+++ eg/scripted_install.pl
@@ -13,7 +13,7 @@ $mod->get;
 my $dir = CPAN::Shell->expand(Distribution => $mod->cpan_file)->dir;
 chdir($dir);
 my $make = $CPAN::Config->{make};
-my $bootstrap = $target ? "--bootstrap=$target" : "--bootstrap"
+my $bootstrap = $target ? "--bootstrap=$target" : "--bootstrap";
 system($^X, 'Makefile.PL', $bootstrap) && exit 1;
 system($make, 'test') && exit 1;
 system($make, 'install') && exit 1;
